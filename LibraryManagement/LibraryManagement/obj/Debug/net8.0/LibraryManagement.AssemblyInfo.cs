56bd6a014996d19b19d39b7a70af685cd169169c
huongduyen3012 <duyen.vanthi.cit20@eiu.edu.vn>
2024-11-05 22:52:20 +0700

    add css to list, readPdf

--- LibraryManagement/LibraryManagement/obj/Debug/net8.0/LibraryManagement.AssemblyInfo.cs
+++ LibraryManagement/LibraryManagement/obj/Debug/net8.0/LibraryManagement.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+033dc1b7860a7aea220dd305c909bb7d83f0bc27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c742c01ba7218f7d781b2bf62abf529bdb5fd58d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
