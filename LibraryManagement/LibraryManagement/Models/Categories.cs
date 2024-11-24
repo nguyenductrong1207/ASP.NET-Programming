8dc0ff278fd146478e68b9cebad7a606eff6bec1
huongduyen3012 <duyen.vanthi.cit20@eiu.edu.vn>
2024-11-24 15:59:45 +0700

    add ui

---
+++ LibraryManagement/LibraryManagement/Models/Categories.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagement.Models
+{
+    public class Categories
+    {
+        [Key]
+        public int CategoryId { get; set; } 
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedDate { get; set; } = DateTime.Now;
+
+        public bool IsActive { get; set; } = true;
+
+        public string Avatar { get; set; }
+    }
+}
--- LibraryManagement/LibraryManagement/obj/Debug/net8.0/LibraryManagement.AssemblyInfo.cs
+++ LibraryManagement/LibraryManagement/obj/Debug/net8.0/LibraryManagement.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88723c081e94316f2d00f8c13806174d525516a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a61b5b6739add3f5fbb738c4dd1555a1e8ee795e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
